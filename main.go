9105b8b6df3def9213e2c96b5d4f992f4512491b
Кошелев Евгений Игоревич <yikoshe1@mts.ru>
2019-11-21 14:55:12 +0300

    code refactored

--- main.go
+++ main.go
@@ -7,60 +7,81 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	yaml "gopkg.in/yaml.v2"
 )
 
-var (
-	directory   = flag.String("directory", "", "directory with target subdirectories")
-	targetsFile = flag.String("targets", "", "file with separate target directories")
-	timeout     = flag.Int64("timeout", 15, "interval (seconds) of checking the number of files in the target directory")
-	port        = flag.String("port", ":9095", "port of return of metrics")
-	handler     = flag.String("handler", "/metrics", "handler for which metrics will be available")
-	count       = flag.String("count", "all", "counting what to do: only files, only folders, everything (can take: files || dirs || all)")
-)
+type parameters struct {
+	Directory, TargetsFile, Port, Handler, Count string
+	Timeout                                      time.Duration
+}
 
 type targetList struct {
 	TargetDirectories []string `yaml:"targetDirectories"`
 }
 
-func checkFlags() {
+func (p *parameters) checkFlags(timer string) {
 
-	if *directory == "" && *targetsFile == "" {
-		log.Fatalln("One or more flags (-directory, -targets) for target directories are not specified")
-	} else if *directory != "" {
-		openSubdirectories(*directory)
-	} else if *targetsFile != "" {
-		file, err := ioutil.ReadFile(*targetsFile)
+	var err error
+
+	if timer == "" {
+		log.Printf("Flag '-timeout' is empty: set default timeout - 15s\n")
+		p.Timeout = time.Duration(15 * time.Second)
+	} else {
+		p.Timeout, err = time.ParseDuration(timer)
 		if err != nil {
-			log.Fatalln("Failed read configuration file: ", err)
+			log.Printf("Failed parse value of flag '-timeout': %s, set default timeout - 15s\n", err.Error())
+			p.Timeout = time.Duration(15 * time.Second)
+		}
+	}
+
+	if p.Directory == "" && p.TargetsFile == "" {
+		log.Fatalln("One or more flags (-directory, -targets) for target directories are not specified")
+	} else if p.Directory != "" {
+		p.openSubdirectories()
+	} else if p.TargetsFile != "" {
+		file, err := ioutil.ReadFile(p.TargetsFile)
+		if err != nil {
+			log.Fatalf("Failed read configuration file: %s", err.Error())
 		}
 		var tl targetList
 		if err = yaml.Unmarshal(file, &tl); err != nil {
-			log.Fatalln("Failed unmarshal targets file: ", err)
+			log.Fatalf("Failed unmarshal targets file: %s", err.Error())
 		}
-		openDirectory(tl.TargetDirectories)
+		p.openDirectory(tl.TargetDirectories)
 	}
 }
 
 func main() {
 
+	var (
+		params parameters
+		timer  string
+	)
+
+	flag.StringVar(&params.Directory, "directory", "", "directory with target subdirectories")
+	flag.StringVar(&params.TargetsFile, "targets", "", "file with separate target directories")
+	flag.StringVar(&timer, "timeout", "", "interval (seconds) of checking the number of files in the target directory")
+	flag.StringVar(&params.Port, "port", ":9095", "port of return of metrics")
+	flag.StringVar(&params.Handler, "handler", "/metrics", "handler for which metrics will be available")
+	flag.StringVar(&params.Count, "count", "all", "counting what to do: only files, only folders, everything (can take: files || dirs || all)")
+
 	flag.Parse()
 
 	// prometheus handler
-	http.Handle(*handler, promhttp.Handler())
+	http.Handle(params.Handler, promhttp.Handler())
 
 	go func() {
-		if err := http.ListenAndServe(*port, nil); err != nil {
-			log.Fatalf("Failed to set http listener: %s", err)
-			os.Exit(1)
+		if err := http.ListenAndServe(params.Port, nil); err != nil {
+			log.Fatalf("Failed to set http listener: %s\n", err.Error())
 		}
 	}()
 
 	log.Printf("Started work!\n")
 
-	checkFlags()
+	params.checkFlags(timer)
 
 	sgnl := make(chan os.Signal)
 	signal.Notify(sgnl, os.Interrupt, os.Kill)
--- work.go
+++ work.go
@@ -7,53 +7,52 @@ import (
 	"time"
 )
 
-func openDirectory(directories []string) {
+func (p *parameters) openDirectory(directories []string) {
 
 	log.Printf("Target directories:\n")
 
 	for _, dir := range directories {
-		go checkNumber(dir)
+		go p.checkNumber(dir)
 	}
-
 }
 
-func openSubdirectories(directory string) {
+func (p *parameters) openSubdirectories() {
 
-	log.Printf("Target subdirectories in \"%s\":\n", directory)
+	log.Printf("Target subdirectories in '%s':\n", p.Directory)
 
-	dir, err := ioutil.ReadDir(directory)
+	dir, err := ioutil.ReadDir(p.Directory)
 	if err != nil {
-		log.Fatalf("Failed read directory: %s\n", err)
+		log.Fatalf("Failed read directory: %s\n", err.Error())
 	}
 
 	for _, item := range dir {
 		if item.IsDir() {
-			go checkNumber(path.Join(directory, item.Name()))
+			go p.checkNumber(path.Join(p.Directory, item.Name()))
 		}
 	}
 }
 
-func checkNumber(directory string) {
+func (p *parameters) checkNumber(directory string) {
 
-	switch *count {
+	switch p.Count {
 	case "all":
 
-		log.Printf("\"%s\"\n", directory)
+		log.Printf("'%s'\n", directory)
 		for {
 			dir, err := ioutil.ReadDir(directory)
 			if err != nil {
-				log.Fatalf("Failed read directory: %s\n", err)
+				log.Fatalf("Failed read directory: %s\n", err.Error())
 			}
-			numberOfFiles.WithLabelValues(directory, *count).Set(float64(len(dir)))
-			time.Sleep(time.Duration(*timeout) * time.Second)
+			numberOfFiles.WithLabelValues(directory, p.Count).Set(float64(len(dir)))
+			time.Sleep(p.Timeout)
 		}
 	case "files":
 
-		log.Printf("\"%s\"\n", directory)
+		log.Printf("'%s'\n", directory)
 		for {
 			dir, err := ioutil.ReadDir(directory)
 			if err != nil {
-				log.Fatalf("Failed read directory: %s\n", err)
+				log.Fatalf("Failed read directory: %s\n", err.Error())
 			}
 			filesCount := 0
 			for _, item := range dir {
@@ -61,16 +60,16 @@ func checkNumber(directory string) {
 					filesCount++
 				}
 			}
-			numberOfFiles.WithLabelValues(directory, *count).Set(float64(filesCount))
-			time.Sleep(time.Duration(*timeout) * time.Second)
+			numberOfFiles.WithLabelValues(directory, p.Count).Set(float64(filesCount))
+			time.Sleep(p.Timeout)
 		}
 	case "dirs":
 
-		log.Printf("\"%s\"\n", directory)
+		log.Printf("'%s'\n", directory)
 		for {
 			dir, err := ioutil.ReadDir(directory)
 			if err != nil {
-				log.Fatalf("Failed read directory: %s\n", err)
+				log.Fatalf("Failed read directory: %s\n", err.Error())
 			}
 			dirsCount := 0
 			for _, item := range dir {
@@ -78,10 +77,10 @@ func checkNumber(directory string) {
 					dirsCount++
 				}
 			}
-			numberOfFiles.WithLabelValues(directory, *count).Set(float64(dirsCount))
-			time.Sleep(time.Duration(*timeout) * time.Second)
+			numberOfFiles.WithLabelValues(directory, p.Count).Set(float64(dirsCount))
+			time.Sleep(p.Timeout)
 		}
 	default:
-		log.Fatalf("Not valid value of the \"-count\" flag - \"%s\": should be \"files\", \"dirs\" or \"all\" - default.", *count)
+		log.Fatalf("Not valid value of the '-count' flag - '%s': should be 'files', 'dirs' or 'all' - default.", p.Count)
 	}
 }
